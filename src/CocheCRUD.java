641b8a96101963a6399adebbce698a8b71521efe
j7programador <j7programador@gmail.com>
2022-11-23 16:27:31 -0500

    Initial commit

---
+++ src/CocheCRUD.java
@@ -0,0 +1,6 @@
+public interface CocheCRUD {
+
+    public void save();
+    public void findAll();
+    public void delete();
+}
---
+++ src/CocheCRUDImpl.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class CocheCRUDImpl  implements  CocheCRUD{
+
+
+    @Override
+    public void save() {
+        System.out.println("Save");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("FindAll");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Delete");
+    }
+}
